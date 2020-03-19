fa21ef1934fc33df066ff486386becbaa8c75dfa
mattfan00 <kungfumo@gmail.com>
2020-03-19 12:21:46 -0400

    Revert "subject routes"
    This reverts commit ddcf353c87e01eea862c93bd98f81724210b11df.

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var express = require('express')
     app = express()
     mongoose = require('mongoose'),
     bodyParser = require('body-parser')
-    Class = require('./models/class'),
-    Subject = require('./models/subject')
+    Class = require('./models/class')
+
+
     
     
 mongoose.connect("mongodb+srv://mattfan00:spacelf14@cluster0-uxcbq.mongodb.net/test?retryWrites=true&w=majority", {
@@ -18,47 +19,34 @@ app.use(express.static(__dirname + "/public"))
 
 app.set('view engine', 'ejs')
 
+
 app.get("/", (req, res) => {
-  res.redirect("/subjects")
-})
-
-app.get("/subjects", (req, res) => {
-  res.send("hi from the subject page")
-})
-
-app.post("/subjects", (req, res) => {
-  Subject.create(req.body, (err, newSubject) => {
-    res.json(newSubject)
-  })
-})
-
-app.get("/classes", (req, res) => {
   Class.find({}, (err, classes) => {
     res.render('index', {classes: classes})
   })
 })
 
-app.post("/classes", (req, res) => {
+app.post("/", (req, res) => {
   Class.create(req.body, (err, newClass) => {
     res.json(newClass)
   })
 })
 
-app.put("/classes/:classId", (req, res) => {
+app.put("/:classId", (req, res) => {
   Class.findByIdAndUpdate(req.params.classId, req.body, {new:true}, (err, updatedClass) => {
     res.json(updatedClass)
   })
 })
 
 // delete all classes
-app.delete("/classes", (req, res) => {
+app.delete("/", (req, res) => {
   Class.remove({}, (err) => {
     res.send('removed all')
   })
 })
 
 // delete individual class
-app.delete("/classes/:classId", (req, res) => {
+app.delete("/:classId", (req, res) => {
   Class.findByIdAndRemove(req.params.classId, (err, removedClass) => {
     res.json(removedClass)
   })
