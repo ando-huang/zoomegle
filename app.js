ddcf353c87e01eea862c93bd98f81724210b11df
mattfan00 <kungfumo@gmail.com>
2020-03-19 10:45:25 -0400

    subject routes

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var express = require('express')
     app = express()
     mongoose = require('mongoose'),
     bodyParser = require('body-parser')
-    Class = require('./models/class')
-
-
+    Class = require('./models/class'),
+    Subject = require('./models/subject')
     
     
 mongoose.connect("mongodb+srv://mattfan00:spacelf14@cluster0-uxcbq.mongodb.net/test?retryWrites=true&w=majority", {
@@ -19,34 +18,47 @@ app.use(express.static(__dirname + "/public"))
 
 app.set('view engine', 'ejs')
 
-
 app.get("/", (req, res) => {
+  res.redirect("/subjects")
+})
+
+app.get("/subjects", (req, res) => {
+  res.send("hi from the subject page")
+})
+
+app.post("/subjects", (req, res) => {
+  Subject.create(req.body, (err, newSubject) => {
+    res.json(newSubject)
+  })
+})
+
+app.get("/classes", (req, res) => {
   Class.find({}, (err, classes) => {
     res.render('index', {classes: classes})
   })
 })
 
-app.post("/", (req, res) => {
+app.post("/classes", (req, res) => {
   Class.create(req.body, (err, newClass) => {
     res.json(newClass)
   })
 })
 
-app.put("/:classId", (req, res) => {
+app.put("/classes/:classId", (req, res) => {
   Class.findByIdAndUpdate(req.params.classId, req.body, {new:true}, (err, updatedClass) => {
     res.json(updatedClass)
   })
 })
 
 // delete all classes
-app.delete("/", (req, res) => {
+app.delete("/classes", (req, res) => {
   Class.remove({}, (err) => {
     res.send('removed all')
   })
 })
 
 // delete individual class
-app.delete("/:classId", (req, res) => {
+app.delete("/classes/:classId", (req, res) => {
   Class.findByIdAndRemove(req.params.classId, (err, removedClass) => {
     res.json(removedClass)
   })
---
+++ models/subject.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+
+var subjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'Cannot be blank'
+  },
+  classes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Class"
+    }
+  ]
+})
+
+module.export = mongoose.model('Subject', subjectSchema)
