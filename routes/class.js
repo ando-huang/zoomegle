aaf05da6a9ba96ba1eb8176dec441ce383c8217f
mattfan00 <kungfumo@gmail.com>
2020-03-20 15:58:32 -0400

    able to create one class at a time now

--- routes/class.js
+++ routes/class.js
@@ -15,4 +15,22 @@ router.post("/subjects/:subjectId/classes", (req, res) => {
   })
 })
 
+router.get("/classes/new", (req, res) => {
+  Subject.find({}, (err, subjects) => {
+    res.render("postClasses", {subjects: subjects})
+  })
+})
+
+router.post("/classes", (req, res) => {
+  console.log(req.body)
+  const newClass = req.body.newClass
+  Subject.findById(newClass.subjectId, (err, foundSubject) => {
+    Class.create(newClass, (err, newClass) => {
+      foundSubject.classes.push(newClass)
+      foundSubject.save()
+      res.redirect("/")
+    })
+  })
+})
+
 module.exports = router
